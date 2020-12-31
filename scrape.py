b4421229583de01a215c33cdea3ce4eb5a560bc3
Tyler Berry <tyler@berryac.com>
2020-12-30 20:50:24 -0700

    scrape.fetch_static_soup supports params

--- scrape.py
+++ scrape.py
@@ -5,16 +5,16 @@ import requests
 _CACHE = diskcache.Cache('.cache/html')
 
 
-def fetch_static_soup(url):
-  html = _fetch_static(url)
+def fetch_static_soup(url, params=None):
+  html = _fetch_static(url, params)
   return bs4.BeautifulSoup(html, 'html.parser')
 
 
 @_CACHE.memoize()
-def _fetch_static(url):
-  return _fetch(url)
+def _fetch_static(url, params=None):
+  return _fetch(url, params)
 
 
-def _fetch(url):
-  response = requests.get(url)
+def _fetch(url, params=None):
+  response = requests.get(url, params=params)
   return response.content
