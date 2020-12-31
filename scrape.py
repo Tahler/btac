0f3a70bb8166fa22d039eb9c29ea1e3132b57977
Tyler Berry <tyler@berryac.com>
2020-12-30 20:51:39 -0700

    Add types to scrape

--- scrape.py
+++ scrape.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import bs4
 import diskcache
 import requests
@@ -5,16 +7,17 @@ import requests
 _CACHE = diskcache.Cache('.cache/html')
 
 
-def fetch_static_soup(url, params=None):
+def fetch_static_soup(url: str,
+                      params: Dict[str, str] = None) -> bs4.BeautifulSoup:
   html = _fetch_static(url, params)
   return bs4.BeautifulSoup(html, 'html.parser')
 
 
 @_CACHE.memoize()
-def _fetch_static(url, params=None):
+def _fetch_static(url: str, params: Dict[str, str] = None) -> str:
   return _fetch(url, params)
 
 
-def _fetch(url, params=None):
+def _fetch(url: str, params: Dict[str, str] = None) -> str:
   response = requests.get(url, params=params)
   return response.content
