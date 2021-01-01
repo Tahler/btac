f8807608715088d73bb8b9fbd34d531fbe988189
Tyler Berry <tyler@berryac.com>
2021-01-01 12:13:20 -0700

    Output description for markers

--- export.py
+++ export.py
@@ -5,13 +5,15 @@ def to_geojson(out_path, fatalities):
   """Writes a CalTopo-compatible GeoJSON file to out_path."""
   features = []
   for f in fatalities:
-    f = geojson.Feature(
-        geometry=geojson.Point((f.longitude, f.latitude)),
-        properties={
-            'title': f.datetime,
-            # 'description': '',
-            'marker-color': '#00FF00',
-        })
+    description_lines = [f.event_url]
+    if f.forecast_url:
+      description_lines.append(f.forecast_url)
+    f = geojson.Feature(geometry=geojson.Point((f.longitude, f.latitude)),
+                        properties={
+                            'title': f.datetime,
+                            'description': '\n'.join(description_lines),
+                            'marker-color': '#00FF00',
+                        })
     features.append(f)
   feature_collection = geojson.FeatureCollection(features)
   with open(out_path, 'w') as f:
