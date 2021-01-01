f4cbf2d845c45e2ad8ad4bf11d0d7730fcd1275f
Tyler Berry <tyler@berryac.com>
2021-01-01 13:03:39 -0700

    Output to out/fatalities.json

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
+
 import btac
 import export
+
+_OUT_DIR = 'out'
 
 
 def main():
   fatalities = btac.fetch_fatalities()
-  export.to_geojson('out.json', fatalities)
+  os.makedirs(_OUT_DIR, exist_ok=True)
+  export.to_geojson(f'{_OUT_DIR}/fatalities.json', fatalities)
 
 
 if __name__ == "__main__":
